dca168b2f169cde2a0948f63132a331606bf4435
deniskipeles <47624821+deniskipeles@users.noreply.github.com>
2023-06-03 12:36:52 +0300

    Create html2text.cpp

---
+++ html2text.cpp
@@ -0,0 +1,16 @@
+#include "html2text.h"
+
+// Constructor
+HTML2Text::HTML2Text() {}
+
+// Destructor
+HTML2Text::~HTML2Text() {}
+
+// Convert HTML to plain text
+std::string HTML2Text::convertToPlainText(const std::string& html) {
+    // Implement your logic to convert HTML to plain text here
+    // ...
+
+    // Return the converted plain text
+    return "Plain text";
+}
